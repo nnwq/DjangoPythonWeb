f24f96ccfe98db773a30d337271d372aadb63caf
Dmitry <dmitrypomishin@gmail.com>
2023-01-13 16:47:15 +0100

    final view revision

--- Project/simplesimpleapp/views.py
+++ Project/simplesimpleapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Product
 
 
@@ -7,5 +7,11 @@ from .models import Product
 class ProductList(ListView):
     model = Product
     ordering = 'name'   # filter filed
+    template_name = 'products.html'
+    context_object_name = 'products'
+
+
+class ProductDetail(DetailView):
+    model = Product
     template_name = 'products.html'
     context_object_name = 'products'
