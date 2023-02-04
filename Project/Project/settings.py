40c7d3f45f3a450fc769f62e8721d14ec6838323
Dmitry <dmitrypomishin@gmail.com>
2023-02-04 15:38:26 +0100

    middleware test

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
+    'full.mobile.middlewaretest.MobileOrFullMiddleware',
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware'
 ]
 
